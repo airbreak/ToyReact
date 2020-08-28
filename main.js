f1f2d385c015dc5364f1812aa8e70467653b9226
airbreak <jiangjianming_m@163.com>
2020-08-28 17:32:12 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-for(let i of [1,2,3]) {
-  console.log(i)
+import {createElement, Component, render} from './toy-react'
+class MyComponent extends Component {
+  render() {
+    return <div>
+      <h1>My component</h1>
+      {this.children}
+      </div>
+  }
 }
-let a = <div></div>
+render(<MyComponent id="a" class="c">
+  <div>123</div>
+  <div></div>
+  <div></div>
+</MyComponent>, document.body)
